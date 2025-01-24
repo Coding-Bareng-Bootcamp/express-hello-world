ce1faee07d509be17138ee00ba1aab3345788623
sarixfauziah <sarixfauziah@gmail.com>
2025-01-24 21:31:47 +0100

    Routing

--- app.js
+++ app.js
@@ -15,11 +15,23 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+});
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
